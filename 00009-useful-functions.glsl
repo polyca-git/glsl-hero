a92f2e5c01243c33018e4a0f0a47300d2fdcc4d7
polyca-git <faruklittle@yandex.com>
2021-10-20 21:11:30 +0200

    descripting better

--- 00009-useful-functions.glsl
+++ 00009-useful-functions.glsl
@@ -3,15 +3,24 @@
 // Desciption: Information here is based on the online book called https://thebookofshaders.com/
 /*
 Look at thiese examples
-00009-1.png -> y = mod(x,0.5); // return x modulo
-00009-2.png -> y = fract(x); // return only the fraction part of a number
-00009-3.png -> y = ceil(x);  // nearest integer that is greater than or equal to x
-00009-4.png -> y = floor(x); // nearest integer less than or equal to x
-00009-5.png -> y = sign(x);  // extract the sign of x
-00009-6.png -> y = abs(x);   // return the absolute value of x
-00009-7.png -> y = clamp(x,0.0,1.0); // constrain x to lie between 0.0 and 1.0
-00009-8.png -> y = min(0.0,x);   // return the lesser of x and 0.0
-00009-9.png -> y = max(0.0,x);   // return the greater of x and 0.0 
+-modulo of a number is what's left of the number after dividing to the second number as much as possible
+00009-1.png -> y = mod(x,0.5); 
+-fraction of a number is non integer part of a number fract(3.0)=0 fract(-0.98)=-0.98
+00009-2.png -> y = fract(x); 
+-returns higer or equal nearest integer
+00009-3.png -> y = ceil(x);  
+-returns less or equal nearest integer
+00009-4.png -> y = floor(x); 
+-sign extraction, returns -1 if the number is negative, returns 1 if the number i positive
+00009-5.png -> y = sign(x); 
+-returns the number as a positive number
+00009-6.png -> y = abs(x); 
+-clamp is restricting a value, returns min value if x is smaller than min, returns max value if x is higher than max 
+00009-7.png -> y = clamp(x,0.0,1.0); 
+-returns the smaller parameter
+00009-8.png -> y = min(0.0,x); 
+-returns the highr parameter
+00009-9.png -> y = max(0.0,x);  
 
 Useful Links:
 https://thebookofshaders.com/edit.php#05/impulse.frag
